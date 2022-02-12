616b4928847317f5e9efb6e70eeed4df9ad238db
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-12 15:05:41 +0700

    delete user route

--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@ this is social media project , make with nodeJS express for back End and react f
 
 link this course [Here]([https://link](https://www.youtube.com/watch?v=ldGl6L4Vktk&list=PLj-4DlPRT48lXaz5YLvbLC38m25W9Kmqy&index=1&t=1481s&ab_channel=LamaDev))
 
-last learn at 24:27
+last learn at 48:20 about make delete user
--- routes/users.js
+++ routes/users.js
@@ -25,5 +25,19 @@ router.put("/:id", async (req,res) => {
     }
 })
 
+//delete
+router.delete("/:id" , async (req,res) => {
+    if (req.body.userId == req.params.id || req.body.isAdmin) {
+        try{
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json({"message":"user has been deleted"})
+        }catch(err){
+            return res.status(500).json({"message": "error"})
+        }
+    } else {
+        return res.status(500).json({"message": "you can delete only your account"})
+    }
+})
+
 module.exports = router;
 

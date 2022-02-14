09f7a0d0d92d80920746e14685e34dc0fc59dbaa
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-14 20:55:31 +0700

    make a route for get users by id

--- routes/users.js
+++ routes/users.js
@@ -39,5 +39,19 @@ router.delete("/:id" , async (req,res) => {
     }
 })
 
+// get  users
+
+router.get("/:id", async (req,res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        const {password,updatedAt, ...other} = user._doc
+        res.status(200).json(other)
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+
+})
+
 module.exports = router;
 

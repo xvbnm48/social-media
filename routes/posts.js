1713083f788202a174fe27ae252f863102e1cfaa
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-16 23:53:19 +0700

    make like dislike, delete post, create post, and get post

--- routes/posts.js
+++ routes/posts.js
@@ -29,8 +29,46 @@ router.put("/:id", async (req,res) => {
     }
 })
 //delete a post
+router.delete("/:id", async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.id)
+        if(post.userId === req.body.userId){
+            await post.deleteOne()
+            res.status(200).json({message: "Post deleted"})
+        }else {
+            res.status(401).json({message:"you can delete only your posts"})
+        }
+
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+})
 //like a post
+router.put("/:id/like", async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes:req.body.userId}})
+            res.status(200).json({message: "Post liked"})
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}})
+            res.status(200).json({message: "Post unliked"})
+        }
+
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+    
+})
 //get a post
+router.get("/:id", async (req,res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err){
+        res.status(500).json({message:err});
+    }
+})
 //get timeline posts 
 
 module.exports = router;

6e98bc7b74b9bcf661227ad8897bca2f9666a18e
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-17 23:33:09 +0700

    timeline error

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { resetWatchers } = require('nodemon/lib/monitor/watch');
 const Post = require('../models/Post');
-
+const User = require("../models/User")
 
 //create a post
 router.post('/', async (req, res) => {
@@ -58,17 +58,34 @@ router.put("/:id/like", async (req,res) => {
     }catch(err){
         res.status(500).json({message:err});
     }
-    
+
 })
 //get a post
-router.get("/:id", async (req,res) => {
-    try{
-        const post = await Post.findById(req.params.id);
-        res.status(200).json(post);
-    }catch(err){
-        res.status(500).json({message:err});
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
     }
-})
-//get timeline posts 
+  });
+  
+//get timeline posts
+router.get("/timeline/all", async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.body.userId);
+      const userPosts = await Post.find({ userId: currentUser._id });
+      const friendPosts = await Promise.all(
+        currentUser.followings.map((friendId) => {
+          return Post.find({ userId: friendId });
+        })
+      );
+      res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+// 
+
 
 module.exports = router;

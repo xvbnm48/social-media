bda2707ec3e01dae62d7a42909093a7b04192cb9
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-06-10 15:24:07 +0700

    fix wrong password return err after send request

--- routes/auth.js
+++ routes/auth.js
@@ -1,48 +1,51 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
+const router = require("express").Router();
+const mongoose = require("mongoose");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //register
 router.post("/register", async (req, res) => {
-    
+  try {
+    //GEMERATE NEW PASSWORD WITH BCRYPT
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    try{
-        //GEMERATE NEW PASSWORD WITH BCRYPT
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    //CREATE NEW USER
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-        //CREATE NEW USER
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
-        });
-
-        //save user and response
-        const user = await newUser.save();
-        res.status(200).json(user)
-    } catch(err){
-        console.log(err)
-        res.status(500).json(err)
-    }
-})
+    //save user and response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 //login
 router.post("/login", async (req, res) => {
-    try{
-        const user = await User.findOne({email: req.body.email});
-        !user && res.status(400).json({message: "User not found"});
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
 
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        !validPassword && res.status(400).json({message: "Invalid password"});
-        res.status(200).json(user);
-    }catch(err){
-        console.log(err)
-        res.status(500).json(err)
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    // !validPassword && res.status(400).json("wrong password");
+    if (!validPassword) {
+      return res.status(400).json("wrong password");
     }
 
-})
+    return res.status(200).json(user);
+    // return;
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
-
